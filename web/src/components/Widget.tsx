02c5eb993e130bf8a7bff9d2fa60faeea80310be
MarcioRMed <contato.microinfo@gmail.com>
2022-05-04 00:08:33 -0300

    Mission 1 - completa

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,10 +1,31 @@
 import { ChatTeardropDots } from "phosphor-react";
+import { useState } from 'react'
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
+    
+
+    // const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+    
+    // function toggleWidgetVisibility(){
+    //     setIsWidgetOpen(!isWidgetOpen)
+    //}        
+    
     return (
-        <button className="bg-violet-500 rounded-full px-3 h-12 text-white">
-            <ChatTeardropDots className="w-6 h-6" />
-        </button>
+        <Popover className="absolute bottom-5 right-5">
+            <Popover.Panel>Hello World</Popover.Panel>
+
+            {/* { isWidgetOpen ? <p>Hello World</p> : null } */}
+            {/* { isWidgetOpen && <p>Hello World</p>} */}
+
+            <Popover.Button  className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group" >
+                <ChatTeardropDots className="w-6 h-6" />
+            
+            <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+                <span className='pl-2'></span> 
+                Feedback</span>
+            </Popover.Button>
+        </Popover>
     );
 }
 
