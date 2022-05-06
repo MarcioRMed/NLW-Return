abb7b479e56b1c7e81ada27e88b345e697e39ea8
MarcioRMed <contato.microinfo@gmail.com>
2022-05-06 20:34:51 -0300

    ajust

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,12 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 
 import AppLoading from 'expo-app-loading'
-import {useFonts, Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter'
+
+import {useFonts, 
+  Inter_400Regular, 
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
 
 import { theme } from './src/theme';
 import { Widget } from './src/components/Widget';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded){
+
+    return <AppLoading />
+  }
+
+
   return (
     <View style={{
       flex: 1,
@@ -15,6 +30,7 @@ export default function App() {
 
     }}>
       
+
       
 
       <Widget />
