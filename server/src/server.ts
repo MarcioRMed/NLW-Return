fbe8e4ea107b0da97cbd845f34b3a90ae886f447
MarcioRMed <contato.microinfo@gmail.com>
2022-05-06 01:40:06 -0300

    ajust

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes'
 
 const app = express();
 
+app.use(cors())
 app.use(express.json()) //informa que vai receber json
 app.use(routes) // importa routes de routes.ts
 
