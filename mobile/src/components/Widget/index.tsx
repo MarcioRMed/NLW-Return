a3b43c4a1427a80b692110de2bdf0242774dbc24
MarcioRMed <contato.microinfo@gmail.com>
2022-05-10 23:36:12 -0300

    atualizações

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
-
+import React, { useRef } from 'react';
+import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native'
+import BottomSheet from '@gorhom/bottom-sheet'
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
+
+import { Options } from '../Options';
 
 import {theme} from '../../theme'
-
 import { styles } from './styles';
 
-export function Widget() {
+function Widget() {
+
+  const bottomSheetRef = useRef<BottomSheet>(null) 
+
+  function handleOpen(){
+    bottomSheetRef.current?.expand()
+  }
+
   return (
     <>
         <TouchableOpacity
             style ={styles.button}
+            onPress={handleOpen}
         >
             <ChatTeardropDots 
             size={24}
@@ -19,6 +29,21 @@ export function Widget() {
             color={theme.colors.text_on_brand_color}
             />
         </TouchableOpacity>
+
+        <BottomSheet 
+          ref={bottomSheetRef}
+          snapPoints= {[1,280]}
+          backgroundStyle={styles.modal}
+          handleIndicatorStyle={styles.indicator}
+        >
+
+        <Options/>
+        </BottomSheet>
+        
+
+
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
